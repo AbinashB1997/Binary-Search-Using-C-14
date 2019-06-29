922cb51b1c7a2bbf39b130bd4e6fc866b22d0f62
Abinash Biswal <31894918+AbinashB1997@users.noreply.github.com>
2019-06-30 00:17:31 +0530

    Add files via upload

---
+++ Diameter of binary tree.cpp
@@ -0,0 +1,16 @@
+pair<int, int> heightDiameter(Node * root) 
+{
+    if(!root) {
+        pair<int, int>P;
+        P.first = P.second = 0;
+        return P;
+    }
+    
+    pair<int, int> Left = heightDiameter(root->left);
+    pair<int, int>Right = heightDiameter(root->right);
+    
+    int Height = 1 + max(Left.first, Right.first);
+    int diameter = max(Left.first + Right.first, max(Left.second,Right.second));
+    
+    return make_pair(Height, diameter);
+}
